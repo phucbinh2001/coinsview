4f5a6651b40a14d33aa6660e5f20c9afd6aabf61
phucbinh2001 <phucbinh.2001@gmail.com>
2022-01-20 17:21:12 +0700

    format number

--- src/Pages/CryptoLive/Components/CryptoTable/index.jsx
+++ src/Pages/CryptoLive/Components/CryptoTable/index.jsx
@@ -29,7 +29,41 @@ function CryptoTable() {
     fetchCoinList().then((data) => {
       setcoinList(data);
     });
+
+    const interval = setInterval(
+      () =>
+        fetchCoinList().then((data) => {
+          setcoinList(data);
+        }),
+      5000
+    );
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
+
+  //format long number to M,B
+  const nFormatter = (num, digits) => {
+    const lookup = [
+      { value: 1, symbol: "" },
+      { value: 1e3, symbol: "k" },
+      { value: 1e6, symbol: "M" },
+      { value: 1e9, symbol: "B" },
+      // { value: 1e12, symbol: "T" },
+      // { value: 1e15, symbol: "P" },
+      // { value: 1e18, symbol: "E" },
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var item = lookup
+      .slice()
+      .reverse()
+      .find(function (item) {
+        return num >= item.value;
+      });
+    return item
+      ? (num / item.value).toFixed(2).replace(rx, "$1") + " " + item.symbol
+      : "0";
+  };
 
   const columns = [
     {
@@ -58,23 +92,57 @@ function CryptoTable() {
     },
     {
       Header: "Price",
-      accessor: "rate",
+      Cell: (row) => {
+        return <>{"$" + parseFloat(row.cell.row.original.rate).toFixed(2)}</>;
+      },
     },
     {
       Header: "Market Cap",
-      accessor: "cap",
+      Cell: (row) => {
+        return (
+          <>
+            {"$" + nFormatter(parseFloat(row.cell.row.original.cap, 1))}
+            <br />
+            {row.cell.row.original.cap}
+          </>
+        );
+      },
     },
     {
       Header: "Volume 24h",
-      accessor: "volume",
+      Cell: (row) => {
+        return (
+          <>
+            {"$" + nFormatter(parseFloat(row.cell.row.original.volume, 1))}
+            <br />
+            {row.cell.row.original.volume}
+          </>
+        );
+      },
     },
     {
       Header: "Liquidity",
-      accessor: "pairs",
+      Cell: (row) => {
+        return (
+          <>
+            {"$" + nFormatter(parseFloat(row.cell.row.original.pairs, 1))}
+            <br />
+            {row.cell.row.original.pairs}
+          </>
+        );
+      },
+      // accessor: "pairs",
     },
     {
       Header: "All-time High",
-      accessor: "allTimeHighUSD",
+
+      Cell: (row) => {
+        return (
+          <>
+            {"$" + parseFloat(row.cell.row.original.allTimeHighUSD).toFixed(2)}
+          </>
+        );
+      },
     },
   ];
 
