73faf18a1138fe6f10d728fb2d3e713b66de6ffb
phucbinh2001 <phucbinh.2001@gmail.com>
2022-01-20 15:50:20 +0700

    tích hợp react table

--- src/Pages/CryptoLive/Components/CryptoTable/index.jsx
+++ src/Pages/CryptoLive/Components/CryptoTable/index.jsx
@@ -1,131 +1,179 @@
 import React from "react";
 import { Button, Table } from "react-bootstrap";
+import TableReact from "../TableReact";
 import "./style.scss";
 
 function CryptoTable() {
-  return (
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th colSpan={10}>
-            <div className="d-flex justify-content-end tool">
-              <Button className="m-1" variant="secondary">
-                <ion-icon name="settings-outline"></ion-icon>
-                Customize
-              </Button>
-              <Button className="m-1" variant="secondary">
-                <ion-icon name="copy-outline"></ion-icon> Categories
-              </Button>
-              <Button className="m-1" variant="secondary">
-                <ion-icon name="repeat-outline"></ion-icon>
-                Exchanges
-              </Button>
-              <Button className="m-1 sort" variant="secondary">
-                <ion-icon name="funnel-outline"></ion-icon>
-              </Button>
+  const columns = [
+    {
+      Header: "Coin",
+      // accessor: (d) => `<img src=${d.webp32} /> ${d.name} ${d.code}`,
+      Cell: (row) => {
+        return (
+          <div className="d-flex align-items-center">
+            <img
+              className="coin-avatar"
+              height={34}
+              src={row.cell.row.original.webp32}
+              alt="ds"
+            />
+            <div>
+              <p className="coin-code mb-0"> {row.cell.row.original.code}</p>
+              <p className="coin-name text-mute mb-0">
+                {" "}
+                {row.cell.row.original.name}
+              </p>
             </div>
-          </th>
-        </tr>
-        <tr>
-          <th>#</th>
-          <th>Coin</th>
-          <th>Price</th>
-          <th>Market Cap</th>
-          <th>Volume 24h</th>
-          <th>Liquidity +-2%</th>
-          <th>All-time High</th>
-          <th>1h</th>
-          <th>24h</th>
-          <th>Weekly</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-      </tbody>
-    </Table>
+          </div>
+        );
+      },
+    },
+    {
+      Header: "Price",
+      accessor: "rate",
+    },
+    {
+      Header: "Market Cap",
+      accessor: "cap",
+    },
+    {
+      Header: "Volume 24h",
+      accessor: "volume",
+    },
+    {
+      Header: "Liquidity",
+      accessor: "pairs",
+    },
+    {
+      Header: "All-time High",
+      accessor: "allTimeHighUSD",
+    },
+  ];
+
+  const data = [
+    {
+      name: "Bitcoin",
+      symbol: "₿",
+      color: "#fa9e32",
+      png32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/btc.png",
+      png64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/btc.png",
+      webp32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/btc.webp",
+      webp64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/btc.webp",
+      exchanges: 169,
+      markets: 4411,
+      pairs: 1591,
+      allTimeHighUSD: 68780.77475755227,
+      circulatingSupply: 18934825,
+      totalSupply: 18934825,
+      maxSupply: 21000000,
+      code: "BTC",
+      rate: 41990.408603978874,
+      volume: 13443090144,
+      cap: 795081038595,
+    },
+    {
+      name: "Ethereum",
+      symbol: "Ξ",
+      color: "#282a2a",
+      png32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/eth.png",
+      png64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/eth.png",
+      webp32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/eth.webp",
+      webp64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/eth.webp",
+      exchanges: 208,
+      markets: 4486,
+      pairs: 2369,
+      allTimeHighUSD: 4861.288700810894,
+      circulatingSupply: 119233995,
+      totalSupply: 119233995,
+      maxSupply: null,
+      code: "ETH",
+      rate: 3136.330764611192,
+      volume: 9790858463,
+      cap: 373957246706,
+    },
+    {
+      name: "Binance Coin",
+      color: "#f4bc2c",
+      png32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/bnb.png",
+      png64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/bnb.png",
+      webp32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/bnb.webp",
+      webp64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/bnb.webp",
+      exchanges: 87,
+      markets: 3609,
+      pairs: 3164,
+      allTimeHighUSD: 691.55924082575,
+      circulatingSupply: 165123057,
+      totalSupply: 165123057,
+      maxSupply: 197192382,
+      code: "BNB",
+      rate: 476.2913502412539,
+      volume: 2191069296,
+      cap: 78646683774,
+    },
+    {
+      name: "Tether",
+      color: "#24a37c",
+      png32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/usdt.png",
+      png64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/usdt.png",
+      webp32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/usdt.webp",
+      webp64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/usdt.webp",
+      exchanges: 180,
+      markets: 9408,
+      pairs: 3206,
+      allTimeHighUSD: 1.053,
+      circulatingSupply: 78324569300,
+      totalSupply: 78324569300,
+      maxSupply: null,
+      code: "USDT",
+      rate: 0.9997362513274686,
+      volume: 32596035595,
+      cap: 78303911299,
+    },
+    {
+      name: "USD Coin",
+      color: "#eef4f9",
+      png32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/usdc.png",
+      png64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/usdc.png",
+      webp32:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/usdc.webp",
+      webp64:
+        "https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/usdc.webp",
+      exchanges: 127,
+      markets: 884,
+      pairs: 532,
+      allTimeHighUSD: 1.087,
+      circulatingSupply: 46053346914,
+      totalSupply: 45736281012,
+      maxSupply: null,
+      code: "USDC",
+      rate: 0.9987946781932135,
+      volume: 2011205082,
+      cap: 45997837811,
+    },
+  ];
+
+  return (
+    <>
+      <TableReact columns={columns} data={data} />
+    </>
   );
 }
 
---
+++ src/Pages/CryptoLive/Components/TableReact/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Button, Table } from "react-bootstrap";
+import { useTable } from "react-table";
+
+function TableReact({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  return (
+    <Table striped bordered hover variant="dark" {...getTableProps()}>
+      <thead>
+        <tr>
+          <th colSpan={10}>
+            <div className="d-flex justify-content-end tool">
+              <Button className="m-1" variant="secondary">
+                <ion-icon name="settings-outline"></ion-icon>
+                Customize
+              </Button>
+              <Button className="m-1" variant="secondary">
+                <ion-icon name="copy-outline"></ion-icon> Categories
+              </Button>
+              <Button className="m-1" variant="secondary">
+                <ion-icon name="repeat-outline"></ion-icon>
+                Exchanges
+              </Button>
+              <Button className="m-1 sort" variant="secondary">
+                <ion-icon name="funnel-outline"></ion-icon>
+              </Button>
+            </div>
+          </th>
+        </tr>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default TableReact;
